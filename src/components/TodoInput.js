3f8c129995623e6923abf85fa8096d4a4ee91128
waqar alam <37731772+waqaralam50@users.noreply.github.com>
2020-04-14 14:55:24 -0700

    first commit

---
+++ src/components/TodoInput.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+// hello world
+export default class TodoInput extends Component {
+  render() {
+    const { item, handleChange, handleSubmit, editItem } = this.props;
+    return (
+      <div className="card card-body my-3">
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <div className="input-group-text bg-primary text-white">
+                <i className="fas fa-book" />
+              </div>
+            </div>
+            <input
+              type="text"
+              className="form-control text-capitalize"
+              placeholder="add todo item"
+              value={item}
+              onChange={handleChange}
+            />
+          </div>
+          <button
+            type="submit"
+            disabled={item ? false : true}
+            className={
+              editItem
+                ? "btn btn-block btn-success mt-3"
+                : "btn btn-block btn-primary mt-3 text-uppercase"
+            }
+          >
+            {editItem ? "edit item" : "add item"}
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
